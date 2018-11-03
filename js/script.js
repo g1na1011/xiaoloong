f0685540440bf73179b4e823c3b9ca06df4d35c4
Gina Zhou <ginayizhou@gmail.com>
2018-11-02 21:19:22 -0700

    complete lunch menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-// function load_lunch() {
-//   document.getElementById("content").innerHTML =
-//     '<object type="text/html" data="../lunch-menu.html"></object>';
-// }
-
 $(document).ready(function() {
   $("#lunch-menu").on("click", function() {
     $("#content").load("../lunch-menu.html");
